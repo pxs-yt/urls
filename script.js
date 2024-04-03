a2677bd05842c8360bc3d1a7d7705cbedebc5a27
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-03 19:58:49 +0200

    !

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById("kali-dev").onclick = function () {
 document.getElementById("movies-list").onclick = function () {
     location.href = "https://pxs-yt.github.io/urls/movies-list.html"
 }
+
+
+// movies-list.html buttons
+// Drive 2011
+document.getElementById("drive-2011").onclick = function () {
+    location.href = "https://pxs-yt.github.io/kali-dev/HTML/404.html"
+}
