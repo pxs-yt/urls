b5d9f0364a7dd01dd85934a3736a84ef363faffc
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-04-03 19:48:10 +0200

    -

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("kali-dev").onclick = function () {
     location.href = "https://pxs-yt.github.io/kali-dev/HTML"
 }
+
+document.getElementById("movies-list").onclick = function () {
+    location.href = "https://pxs-yt.github.io/urls/movies-list.html"
+}
